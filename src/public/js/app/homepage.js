78eac53a028c1ca1b0367110689fdd7e06277a1b
Rene Hernandez <rhernandez@onenorth.com>
2017-02-24 09:40:50 -0600

    panel welcome

--- src/public/js/app/homepage.js
+++ src/public/js/app/homepage.js
@@ -1,98 +1,98 @@
 ﻿(function(){
 
-'use strict';
+// 'use strict';
 
 
 
-    var customNextSlideLogic,
-        showLogoPieceTransition,
-        resumeFullpage,
-        isTransitioning =  false,
-        forcedTransition = false,
-        overlayTransitionTiming = 1500,
-        fullTrasitionDelay = overlayTransitionTiming + 600,
+//     var customNextSlideLogic,
+//         showLogoPieceTransition,
+//         resumeFullpage,
+//         isTransitioning =  false,
+//         forcedTransition = false,
+//         overlayTransitionTiming = 1500,
+//         fullTrasitionDelay = overlayTransitionTiming + 600,
 
-        $logoPieceBg = $('.logo-piece-cover > .logo-piece-cover-section-bg');
-
-
-    resumeFullpage = function() {
-        $.fn.fullpage.setAllowScrolling(true);
-        $.fn.fullpage.setKeyboardScrolling(true);
-
-        $('body').removeClass('show-logo');
-
-        isTransitioning = false;
-    };
-
-    showLogoPieceTransition = function(nextSlideIndex, $nextSlide) {
-        $('body').addClass('show-logo');
-
-        var imgUrl = $nextSlide.find('.js-bg').attr('src');
+//         $logoPieceBg = $('.logo-piece-cover > .logo-piece-cover-section-bg');
 
 
-        $logoPieceBg.attr('src', imgUrl);
+//     resumeFullpage = function() {
+//         $.fn.fullpage.setAllowScrolling(true);
+//         $.fn.fullpage.setKeyboardScrolling(true);
 
-        setTimeout(function(){
+//         $('body').removeClass('show-logo');
 
-            $.fn.fullpage.silentMoveTo(nextSlideIndex);
+//         isTransitioning = false;
+//     };
+
+//     showLogoPieceTransition = function(nextSlideIndex, $nextSlide) {
+//         $('body').addClass('show-logo');
+
+//         var imgUrl = $nextSlide.find('.js-bg').attr('src');
 
 
-        }, overlayTransitionTiming/2);
+//         $logoPieceBg.attr('src', imgUrl);
+
+//         setTimeout(function(){
+
+//             $.fn.fullpage.silentMoveTo(nextSlideIndex);
 
 
-        setTimeout(function(){
-            resumeFullpage();
-        }, fullTrasitionDelay);
-    };
+//         }, overlayTransitionTiming/2);
+
+
+//         setTimeout(function(){
+//             resumeFullpage();
+//         }, fullTrasitionDelay);
+//     };
 
     
 
-    customNextSlideLogic = function nextSlide(nextSlideIndex, $nextSlide){
-        isTransitioning = true;
+//     customNextSlideLogic = function nextSlide(nextSlideIndex, $nextSlide){
+//         isTransitioning = true;
         
-        //disable scrolling functionality until we finish transitioning out 
-        $.fn.fullpage.setAllowScrolling(false);
-        $.fn.fullpage.setKeyboardScrolling(false);
+//         //disable scrolling functionality until we finish transitioning out 
+//         $.fn.fullpage.setAllowScrolling(false);
+//         $.fn.fullpage.setKeyboardScrolling(false);
 
-        showLogoPieceTransition(nextSlideIndex, $nextSlide);    
+//         showLogoPieceTransition(nextSlideIndex, $nextSlide);    
         
-    };
+//     };
     
-    $('#fullpage').fullpage({
-        verticalCentered: false,
-        scrollingSpeed: 1000,
-        fitToSection: false,
-        fitToSectionDelay: 5000,
-        normalScrollElementTouchThreshold: 10,
-        touchSensitivity: 10,
-        //animateAnchor: false
-        onLeave: function(index, nextIndex, direction){
+//     $('#fullpage').fullpage({
+//         verticalCentered: false,
+//         scrollingSpeed: 1000,
+//         fitToSection: false,
+//         fitToSectionDelay: 5000,
+//         normalScrollElementTouchThreshold: 10,
+//         touchSensitivity: 10,
+//         //animateAnchor: false
+//         onLeave: function(index, nextIndex, direction){
             
 
-            if (!isTransitioning) {
-                customNextSlideLogic(nextIndex, $(this));
-                return false;
-            }
+//             if (!isTransitioning) {
+//                 customNextSlideLogic(nextIndex, $(this));
+//                 return false;
+//             }
 
            
-        }
-    });
+//         }
+//     });
     
     
-    $.fn.fullpage.setAllowScrolling(false);
-    $.fn.fullpage.setKeyboardScrolling(false);
+//     $.fn.fullpage.setAllowScrolling(false);
+//     $.fn.fullpage.setKeyboardScrolling(false);
 
 
     
-    $(function(){
-        $('body').addClass('dom-loaded');
+//     $(function(){
+//         $('body').addClass('dom-loaded');
 
 
-        setTimeout(function(){
-            $.fn.fullpage.setAllowScrolling(true);
-            $.fn.fullpage.setKeyboardScrolling(true);
-        }, 2000);
-    }); 
+//         setTimeout(function(){
+//             $.fn.fullpage.setAllowScrolling(true);
+//             $.fn.fullpage.setKeyboardScrolling(true);
+//         }, 2000);
+//     }); 
     
 
     
